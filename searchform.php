6bf031bf81661ce5fc4e1bea1c49baadf4bb04e1
sopheaknova <novadesign.dev@gmail.com>
2014-05-20 16:49:26 +0700

    Fixed search url and contact form

--- searchform.php
+++ searchform.php
@@ -7,7 +7,7 @@ $search_text = get_search_query();
 if ( empty( $search_text ) )
 	$search_text = __( 'Search...', SP_TEXT_DOMAIN ); ?>
 <div class="searchbox">
-<form role="search" method="get" id="searchform" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
+<form role="search" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" >
     <input type="text" value="<?php echo $search_text ?>" name="s" id="s" onblur="if (this.value == '')
     {this.value = '<?php echo $search_text ?>';}"
     onfocus="if (this.value == '<?php echo $search_text ?>')
